b51eb8d5a53056b51ba08bcd407bc480f4a868cf
Sameer Shaik <sameershaik209124@gmail.com>
2025-12-16 01:31:51 +0530

    modifiers

--- app/src/main/java/com/example/jetpackcomposelearning/remember.kt
+++ app/src/main/java/com/example/jetpackcomposelearning/remember.kt
@@ -1,5 +1,9 @@
 package com.example.jetpackcomposelearning
 
+import androidx.annotation.ColorRes
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -7,12 +11,16 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.unit.dp
 
 
 @Composable
 fun method(){
     var count by remember { mutableStateOf(0) }
-        Text("$count")
+        Text("$count" , modifier = Modifier.padding(16.dp) , color = colorResource(R.color.teal_700))
+    Spacer(modifier =  Modifier.height(16.dp))
     Button(onClick = {
         count++
     }) {
