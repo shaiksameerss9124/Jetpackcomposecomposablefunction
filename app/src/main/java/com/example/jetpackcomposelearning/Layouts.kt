191abd7c77c3728c26bec574bf3337b94b1e7dd7
Sameer Shaik <sameershaik209124@gmail.com>
2025-12-16 10:45:22 +0530

    Box Layout

--- app/src/main/java/com/example/jetpackcomposelearning/Layouts.kt
+++ app/src/main/java/com/example/jetpackcomposelearning/Layouts.kt
@@ -1,33 +1,56 @@
 package com.example.jetpackcomposelearning
 
+import android.media.ImageWriter
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun coloumnlayout() {
 
-    Row (modifier = Modifier.background(color = Color.Green).fillMaxSize() , verticalAlignment = Alignment.CenterVertically , horizontalArrangement = Arrangement.Center) {
-    Text("Hello Dosto")
-    Text("Hi sameer")
+  /*  Row(
+        modifier = Modifier.background(color = Color.Green).fillMaxSize(),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.Center
+    ) {
+        Text("Hello Dosto")
+        Text("Hi sameer")
         Button(onClick = {}) {
-            Text("Something" , color= Color.Green)
+            Text("Something", color = Color.Green)
         }
+    }*/
+
+    Box(modifier = Modifier.fillMaxSize()) {
+        Image(
+            painter = painterResource(R.drawable.myfavoriteimage),
+            contentDescription = null, modifier = Modifier.width(100.dp).height(100.dp).align (Alignment.Center )
+        )
+        Text("sameer overlay text" ,color = Color.Black, modifier = Modifier.align(Alignment.Center))
+    }
 }
 
-}
+
+
 
 @Composable
 @Preview(showSystemUi = true)
 fun colomnlayputexample(modifier: Modifier = Modifier){
     coloumnlayout()
+
 }
