a5c2dfd883cceba2f22c518a3fc2026355541307
Sameer Shaik <sameershaik209124@gmail.com>
2025-12-16 11:59:39 +0530

    Responsive design

--- app/src/main/java/com/example/jetpackcomposelearning/LazyLayout.kt
+++ app/src/main/java/com/example/jetpackcomposelearning/LazyLayout.kt
@@ -1,23 +1,47 @@
 package com.example.jetpackcomposelearning
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import java.nio.file.WatchEvent
 
 @Composable
 fun lazylayout() {
+    //LazyColumn
+    /*
     LazyRow {
         items(count =100){
             index ->
             Text("Item$index" , modifier = Modifier.padding(8.dp) )
         }
     }
-    
+    */
+
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        //content = TODO()
+
+    ){
+            Button(onClick =  {}, modifier = Modifier.fillMaxWidth() ) {
+                Text("Something")
+            }
+    }
+
+
 }
 @Composable
 @Preview
